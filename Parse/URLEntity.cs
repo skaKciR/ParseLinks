53f42821ade286bf06d9523881dd571ccbe163f5
Kirill <Kirill@DESKTOP-HELUB8V>
2024-02-04 14:57:53 +0300

    Добавьте файлы проекта.

---
+++ Parse/URLEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse
+{
+    public class URLEntity
+    {
+        public string URL { get; set; }
+        public string HTML { get; set; }
+        public string Text { get; set; }
+        public List<string> Links { get; set; }
+
+    }
+}
